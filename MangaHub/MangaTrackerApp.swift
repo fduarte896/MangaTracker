1ef184ea41cc1686e5cc4e5dcba1a1eb1a0d0f91
fduarte896 <66535395+fduarte896@users.noreply.github.com>
2024-07-08 18:21:10 -0500

    First cleaning session, adjusting the app naming and deleting files from the network folder.

---
+++ MangaHub/MangaTrackerApp.swift
@@ -0,0 +1,45 @@
+//
+//  MangaTrackerApp.swift
+//  MangaHub
+//
+//  Created by Sebastian Alzate on 4/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MangaTrackerApp: App {
+    
+    @AppStorage("isFirstLaunch") var isFirstLaunch: Bool = true
+    @State private var showSplash = true
+    
+    var body: some Scene {
+        WindowGroup {
+            
+            ZStack {
+                if showSplash {
+                    MangaSplashScreenView()
+                        .transition(.opacity)
+                } else {
+                    
+                    if isFirstLaunch {
+                        PagerView(isFirstLaunch: $isFirstLaunch)
+                    } else {
+                        MainTabItemView()
+                    }
+                }
+            }
+            .onAppear {
+                Task {
+                    try? await Task.sleep(for: .milliseconds(1000))
+                    withAnimation(.easeOut(duration: 1)) {
+                        showSplash = false
+                    }
+                }
+            }
+        }
+    }
+}
+    
+    
+    
