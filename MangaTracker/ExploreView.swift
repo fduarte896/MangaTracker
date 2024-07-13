8c78206538d9442dde5658110b362036fb751a93
fduarte896 <66535395+fduarte896@users.noreply.github.com>
2024-07-12 20:16:55 -0500

    Last checkpoint before trying to add a bucket list view and merging homeview y categories

---
+++ MangaTracker/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  MangaTracker
+//
+//  Created by Felipe Duarte on 12/07/24.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
