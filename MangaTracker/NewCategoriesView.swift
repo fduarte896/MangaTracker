aba0901fb7aca3200a92d0c38a422a08121943f1
fduarte896 <66535395+fduarte896@users.noreply.github.com>
2024-07-16 08:19:33 -0500

    NewExploreView now works with the all mangas, best mangas and the three categories buttons. HomeView and CateogoriesView were recplaced by them.

---
+++ MangaTracker/NewCategoriesView.swift
@@ -0,0 +1,18 @@
+//
+//  NewCategoriesView.swift
+//  MangaTracker
+//
+//  Created by Felipe Duarte on 13/07/24.
+//
+
+import SwiftUI
+
+struct NewCategoriesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewCategoriesView()
+}
