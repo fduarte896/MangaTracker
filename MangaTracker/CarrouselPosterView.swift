7ba2d14f90d50faab066e7db174aa72dad42366d
fduarte896 <66535395+fduarte896@users.noreply.github.com>
2024-07-22 18:03:59 -0500

    Commit before editing detail views.

---
+++ MangaTracker/CarrouselPosterView.swift
@@ -0,0 +1,18 @@
+//
+//  CarrouselPosterView.swift
+//  MangaTracker
+//
+//  Created by Felipe Duarte on 20/07/24.
+//
+
+import SwiftUI
+
+struct CarrouselPosterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarrouselPosterView()
+}
---
+++ MangaTracker/tarjetas.swift
@@ -0,0 +1,18 @@
+//
+//  tarjetas.swift
+//  MangaTracker
+//
+//  Created by Felipe Duarte on 20/07/24.
+//
+
+import SwiftUI
+
+struct tarjetas: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    tarjetas()
+}
