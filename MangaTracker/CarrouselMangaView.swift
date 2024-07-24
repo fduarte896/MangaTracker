bfe2f99dee999cd8a50a9b7c23870ae0e71e5db7
fduarte896 <66535395+fduarte896@users.noreply.github.com>
2024-07-24 15:33:26 -0500

    Carousel done and section with mangas you own y bucket list tracking wont move even if the user scrolls.

---
+++ MangaTracker/CarrouselMangaView.swift
@@ -0,0 +1,18 @@
+//
+//  CarrouselMangaView.swift
+//  MangaTracker
+//
+//  Created by Felipe Duarte on 23/07/24.
+//
+
+import SwiftUI
+
+struct CarrouselMangaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarrouselMangaView()
+}
