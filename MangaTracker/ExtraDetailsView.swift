69d4b734c209099e0e9e7d49f7d62149819d8515
fduarte896 <66535395+fduarte896@users.noreply.github.com>
2024-08-02 10:21:54 -0500

    DetailView Finished. Next step: My Collection Detail

---
+++ MangaTracker/ExtraDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  ExtraDetailsView.swift
+//  MangaTracker
+//
+//  Created by Felipe Duarte on 31/07/24.
+//
+
+import SwiftUI
+
+struct ExtraDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExtraDetailsView()
+}
