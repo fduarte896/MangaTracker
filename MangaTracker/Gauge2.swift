2ed975ae4588033ef7c60f1626e9c23a2b00f4df
fduarte896 <66535395+fduarte896@users.noreply.github.com>
2024-08-03 09:54:28 -0500

    My Collection Detail View completed. Next step: My Bucket List Detail View

---
+++ MangaTracker/Gauge2.swift
@@ -0,0 +1,18 @@
+//
+//  Gauge2.swift
+//  MangaTracker
+//
+//  Created by Felipe Duarte on 2/08/24.
+//
+
+import SwiftUI
+
+struct Gauge2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Gauge2()
+}
---
+++ MangaTracker/GaugeView.swift
@@ -0,0 +1,18 @@
+//
+//  GaugeView.swift
+//  MangaTracker
+//
+//  Created by Felipe Duarte on 2/08/24.
+//
+
+import SwiftUI
+
+struct GaugeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GaugeView()
+}
