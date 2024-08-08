a4c9ae40491ccc8cf3c217b7b95d6959eedd2b42
fduarte896 <66535395+fduarte896@users.noreply.github.com>
2024-08-08 16:52:01 -0500

    iPad design started.

---
+++ MangaTracker/PruebaDinamica.swift
@@ -0,0 +1,18 @@
+//
+//  PruebaDinamica.swift
+//  MangaTracker
+//
+//  Created by Felipe Duarte on 8/08/24.
+//
+
+import SwiftUI
+
+struct PruebaDinamica: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PruebaDinamica()
+}
