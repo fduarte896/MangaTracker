652a33bd4798cdb3231e0bec30665cb253e94102
fduarte896 <66535395+fduarte896@users.noreply.github.com>
2024-08-12 08:41:55 -0500

    App colors defined and placed in the appropriate extension. Detail View optimized for iPad Explore view optimized for iPad ExtraDetails optimized for iPad

---
+++ MangaTracker/MangaTrackerColors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  MangaTracker
+//
+//  Created by Felipe Duarte on 9/08/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+}
---
+++ MangaTracker/UIDeviceExtension.swift
@@ -0,0 +1,18 @@
+//
+//  UIDeviceExtension.swift
+//  MangaTracker
+//
+//  Created by Felipe Duarte on 9/08/24.
+//
+
+import SwiftUI
+
+struct UIDeviceExtension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIDeviceExtension()
+}
